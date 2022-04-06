e6deaf1393a0b32a1b50cc82fd84b040d238fcd8
Zulema Campos <zulemacampos@Zulemas-Air.fios-router.home>
2022-04-06 10:54:51 -0400

    initial commit

---
+++ src/test/java/com/campos/zulema/chistes/api/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campos.zulema.chistes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
